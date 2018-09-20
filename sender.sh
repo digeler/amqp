dcace7fb4b17136c08a771762fe3fb8b5757dcfa
Dinor Geler <digeler@users.noreply.github.com>
2018-09-20 07:32:33 +0300

    Create sender.sh

---
+++ sender.sh
@@ -0,0 +1,10 @@
+export EH_TEST_NAMESPACE=jbhubevent
+export AZURE_SUBSCRIPTION_ID=928f4e7e-2c28-4063-a56e-6f1e6f2bb73c
+export AZURE_TENANT_ID=72f988bf-86f1-41af-91ab-2d7cd011db47
+export AZURE_CLIENT_ID=3ea6e6e4-3f2d-4ddd-9ece-9c035cbd951e
+export AZURE_CLIENT_SECRET=******
+export EH_TEST_NAME=tester
+export EH_TEST_SAS_POLICY_NAME=RootManageSharedAccessKey
+export EH_TEST_SAS_POLICY_KEY=****5o52OsFHlhDEmGvir4bq04vgHAInvryGA=
+export EH_TEST_CONSUMER_GROUP="\$Default"
+/go/src/github.com/openenergi/go-event-hub/sender
